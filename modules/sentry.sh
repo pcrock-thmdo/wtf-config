155fc38e626fc9590f1905742d71b64dad5dabac
Philip Crockett <philip.crockett@thermondo.de>
2024-11-27 09:39:59 +0100

    get sentry functional

--- modules/sentry.sh
+++ modules/sentry.sh
@@ -3,9 +3,17 @@
 set -Eeuo pipefail
 
 readonly ORG="thermondo"
-readonly PROJECT="thermondo-backend"
-readonly STATS_PERIOD="14d"
-readonly QUERY="is%3Aunresolved+assigned%3A%5B%23field_service%2C%23svmx%5D"
+readonly STATS_PERIOD="30d"
+
+# tip:
+#
+# ```python
+# from urllib.parse import urlencode
+# urlencode("is:unresolved assigned_or_suggested:[me, my_teams]")
+# ```
+#
+readonly QUERY="is%3Aunresolved+assigned_or_suggested%3A%5Bme%2C+my_teams%5D"
+
 readonly SENTRY_KEY_ID="com.philcrockett.wtfutil.sentry"
 
 get_sentry_api_key() {
@@ -15,6 +23,6 @@ get_sentry_api_key() {
 SENTRY_API_KEY="$(get_sentry_api_key)"
 readonly SENTRY_API_KEY
 
-curl "https://sentry.io/api/0/projects/${ORG}/${PROJECT}/issues/?statsPeriod=${STATS_PERIOD}&query=${QUERY}" \
+curl "https://sentry.io/api/0/organizations/${ORG}/issues/?statsPeriod=${STATS_PERIOD}&query=${QUERY}&environment=prod&environment=production" \
     -H "Authorization: Bearer ${SENTRY_API_KEY}" \
-    | jq ".[] | [.count, .userCount, (.lastSeen | .[0:10]), .title] | join(\" \")"
+    | jq --raw-output ".[] | [.count, .userCount, (.lastSeen | .[0:10]), .title] | join(\" \")"
